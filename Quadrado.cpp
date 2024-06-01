ba52d46a265d057845001e549d9d275c88129a53
Bruno Nascimento Maciel <112732163+Star-Studies@users.noreply.github.com>
2024-06-01 09:43:45 -0400

    Created Quadrado.cpp

---
+++ Quadrado.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  cout << "Digite um número: "; 
+  cin >> x; // Get user input from the keyboard
+  cout << "O quadrado do seu número é: " << x*x; 
+}
